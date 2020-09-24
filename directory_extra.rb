76ed42701a5ce2787e8b046b0490c82c7a1a3603
charlie-galb <galbraith.charlie@gmail.com>
2020-09-24 09:21:33 +0100

    Add .delete method as alternative to ,chomp (for sake of experimentation). Add .center method to align output

--- directory_extra.rb
+++ directory_extra.rb
@@ -2,14 +2,14 @@ def input_students
 
   cohorts = ["september", "october", "november", "december"]
 
-  puts "Enter the name of a student to add them to the system"
-  puts "To finish, just hit return three times"
+  puts "Enter the name of a student to add them to the system".center(100)
+  puts "To finish, just hit return three times".center(100)
   name = gets.chomp
-  puts "Please enter the student's country of origin"
+  puts "Please enter the student's country of origin".center(100)
   country = gets.chomp
   while true
-    puts "Please enter the student's cohort"
-    cohort = gets.chomp
+    puts "Please enter the student's cohort".center(100)
+    cohort = gets.delete "\n"
       if cohorts.include?(cohort)
         break
       elsif cohort.empty?
@@ -24,20 +24,20 @@ def input_students
 
     students << {name: name, country: country, cohort: cohort.downcase.to_sym}
     if students.length == 1
-      puts "Now we have #{students.count} student"
+      puts "Now we have #{students.count} student".center(100)
     else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{students.count} students".center(100)
     end
 
-    puts "Please enter the name of a student"
-    puts "To finish, just hit return without typing anything"
+    puts "Please enter the name of a student".center(100)
+    puts "To finish, just hit return without typing anything".center(100)
     name = gets.chomp
 
     if !name.empty?
-      puts "Please enter the student's country of origin"
+      puts "Please enter the student's country of origin".center(100)
       country = gets.chomp
       if !country.empty?
-          puts "Please enter the student's cohort"
+          puts "Please enter the student's cohort".center(100)
           while true
           cohort = gets.chomp
             if cohorts.include?(cohort)
@@ -52,8 +52,8 @@ def input_students
   students
 end
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(100)
+  puts "-------------".center(100)
 end
 def sort_students(students)
   students.sort_by { |student| student[:cohort] }
@@ -61,15 +61,15 @@ end
 def print(students)
   counter = 0
   until counter == students.length
-    puts "#{counter+1}. #{students[counter][:name]}, Country of origin: #{students[counter][:country]} (#{students[counter][:cohort]} cohort)"
+    puts "#{counter+1}. #{students[counter][:name]}, Country of origin: #{students[counter][:country]} (#{students[counter][:cohort]} cohort)".center(100)
     counter += 1
   end
 end
 def print_footer(students)
   if students.length == 1
-    puts "Overall, we have #{students.count} great student"
+    puts "Overall, we have #{students.count} great student".center(100)
   else
-    puts "Overall, we have #{students.count} great students"
+    puts "Overall, we have #{students.count} great students".center(100)
   end
 end
 
