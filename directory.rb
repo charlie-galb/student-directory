62627c34e27b7c0d40b80b979b9ebd8f03a584a1
charlie-galb <galbraith.charlie@gmail.com>
2020-09-24 10:33:29 +0100

    Alter try_load_students method to load from student.csv if no filename provided on startup

--- directory.rb
+++ directory.rb
@@ -91,8 +91,10 @@ end
 
 def try_load_students
   filename = ARGV.first
-  return if filename.nil?
-  if File.exists?(filename)
+  if filename.nil?
+    load_students
+    puts "Loaded #{@students.count} from 'students.csv'"
+  elsif File.exists?(filename)
     load_students(filename)
     puts "Loaded #{@students.count} from #{filename}"
   else
