f62e4bcee8ec85b2e12b579511811ed27d171ff0
Charlie Galbraith <galbraith.charlie@gmail.com>
2020-09-29 12:37:48 +0100

    Add Student class, refactoring according to principles of encapsulation and SRP. Still debugging.

--- directory_extra.rb
+++ directory_extra.rb
@@ -1,56 +1,66 @@
-def input_students
+class Student
 
-  cohorts = ["september", "october", "november", "december"]
-
-  puts "Enter the name of a student to add them to the system".center(100)
-  puts "To finish, just hit return three times".center(100)
-  name = gets.chomp
-  puts "Please enter the student's country of origin".center(100)
-  country = gets.chomp
-  while true
-    puts "Please enter the student's cohort".center(100)
-    cohort = gets.delete "\n"
-      if cohorts.include?(cohort)
-        break
-      elsif cohort.empty?
-        break
-      end
+  def get_name
+    @name = gets.chomp
   end
 
+  def get_country
+    @country = gets.chomp
+  end
 
-  students = []
-
-  while !name.empty? do
-
-    students << {name: name, country: country, cohort: cohort.downcase.to_sym}
-    if students.length == 1
-      puts "Now we have #{students.count} student".center(100)
-    else
-      puts "Now we have #{students.count} students".center(100)
-    end
-
-    puts "Please enter the name of a student".center(100)
-    puts "To finish, just hit return without typing anything".center(100)
-    name = gets.chomp
-
-    if !name.empty?
-      puts "Please enter the student's country of origin".center(100)
-      country = gets.chomp
-      if !country.empty?
-          puts "Please enter the student's cohort".center(100)
-          while true
-          cohort = gets.chomp
-            if cohorts.include?(cohort)
-              break
-            end
-          end
+  def get_cohort
+    cohorts = ["september", "october", "november", "december"]
+    while true do
+      @cohort = gets.chomp
+      if cohorts.include?(@cohort)
+        break
       end
+    end
+  end
 
+  def name
+    @name
+  end
 
+  def country
+    @country
+  end
+
+  def cohort
+    @cohort
+  end
+
+end
+
+def input_students
+
+  while true do
+
+    puts "Enter the name of a student to add them to the system".center(100)
+    puts "To finish, just hit return three times".center(100)
+    student = Student.new
+    student.get_name
+    puts "Please enter the student's country of origin".center(100)
+    student.get_country
+    puts "Please enter the student's cohort".center(100)
+    student.get_cohort
+
+    students = []
+
+    if student.name.empty?
+      break
+    else
+        students << {name: student.name, country: student.country, cohort: student.cohort.downcase.to_sym}
+      if students.length == 1
+        puts "Now we have #{students.count} student".center(100)
+      else
+        puts "Now we have #{students.count} students".center(100)
+      end
     end
   end
   students
 end
+
 def print_header
   puts "The students of Villains Academy".center(100)
   puts "-------------".center(100)
@@ -65,6 +75,7 @@ def print(students)
     counter += 1
   end
 end
+
 def print_footer(students)
   if students.length == 1
     puts "Overall, we have #{students.count} great student".center(100)
